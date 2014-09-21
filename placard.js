9b206f8a880d71f3fc641f638d6f988c9eed2108
Taylor Smith <mail@taylorsmith.net>
2014-09-21 10:33:44 -0600

    Add initial map and point functionality

---
+++ placard.js
@@ -0,0 +1,38 @@
+(function(window, google) {
+  'use strict';
+
+  var Placard = {
+    Map: function(el, config) {
+      this.el = el;
+      this.config = config;
+      this._map = this._createMap();
+    },
+
+    Point: function(point) {
+      this.lat = point.lat;
+      this.lng = point.lng;
+    }
+  };
+
+  Placard.Map.prototype._createMap = function() {
+    return new google.maps.Map(document.getElementById(this.el), this.config);
+  };
+
+  Placard.Map.prototype.addPoint = function(point) {
+    var newPoint = new Placard.Point(point, this);
+    newPoint.addToMap(this);
+
+    return this;
+  };
+
+  Placard.Point.prototype.addToMap = function(map) {
+    new google.maps.Marker({
+      position: new google.maps.LatLng(this.lat, this.lng),
+      map: map._map
+    });
+
+    return this;
+  };
+
+  window.Placard = Placard;
+})(window, google);
